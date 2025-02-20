a856333cb2966ef14c0eab9a3a7d2a278c310a6f
korotkov_egor <korotkovegor10@gmail.com>
2025-02-20 19:54:58 +0300

    add test for get_divs_with_data func

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,7 +1,11 @@
 import pytest
 from bs4 import BeautifulSoup
 
-from products_assistent.products import get_product_cards, del_offer_feed_if_there_is
+from products_assistent.products import (
+    get_product_cards,
+    del_offer_feed_if_there_is,
+    get_divs_with_data,
+)
 
 
 @pytest.fixture
@@ -22,7 +26,7 @@ def test_get_prd_cards_err(prd_cards_soup_err):
         pass_test = True
     finally:
         if not pass_test:
-            err_text = 'не выбрасывает ошибку при отсутствии карт'
+            err_text = "не выбрасывает ошибку при отсутствии карт"
         assert pass_test, err_text
 
 
@@ -35,26 +39,61 @@ def del_offer_soup():
 
 
 def test_del_offer_feed(del_offer_soup):
-    soup_array = del_offer_soup
-    print(soup_array[2].div.get("data-apiary-widget-name") == "@monetize/PremiumIncut")
+    (
+        first_offer_test,
+        second_offer_test,
+        empty_dell_test,
+        first_offer_class_test,
+        second_offer_class_test,
+    ) = del_offer_soup
 
     try:
-        first_test = del_offer_feed_if_there_is([soup_array[0]])
+        first_test = del_offer_feed_if_there_is([first_offer_test])
     except IndexError:
-        err_text = 'берет не первых эллемент'
+        err_text = "берет не первых эллемент"
         assert True, err_text
 
     err_text = "не удаляет с атрибутом: @monetize/IncutConstructor/Premium"
     assert len(first_test) == 0, err_text
 
     err_text = "не удаляет с атрибутом: @monetize/PremiumIncut"
-    assert len(del_offer_feed_if_there_is([soup_array[1]])) == 0, err_text
+    assert len(del_offer_feed_if_there_is([second_offer_test])) == 0, err_text
 
-    err_text = "удяляет даже, если это не список предложений"
-    assert len(del_offer_feed_if_there_is([soup_array[2]])) == 1, err_text
+    err_text = "удяляет даже, если data-apiary-widget-name не верный атрибут"
+    assert len(del_offer_feed_if_there_is([empty_dell_test])) == 1, err_text
 
-    err_text = "удяляет даже, если это не список предложений"
-    assert len(del_offer_feed_if_there_is([soup_array[3]])) == 1, err_text
+    err_text = "удяляет даже, если class=@monetize/IncutConstructor/Premium"
+    assert len(del_offer_feed_if_there_is([first_offer_class_test])) == 1, (
+        err_text
+    )
+
+    err_text = "удяляет даже, если class=@monetize/PremiumIncut"
+    assert len(del_offer_feed_if_there_is([second_offer_class_test])) == 1, (
+        err_text
+    )
+
+
+@pytest.fixture
+def divs_with_data_soup():
+    with open("tests/fixtures/yandex/divs_with_data.html", "r") as f:
+        soup = BeautifulSoup(f, "html.parser")
+
+    return soup.find_all("div", recursive=False)
+
+
+def test_get_divs_with_data(divs_with_data_soup):
+    none_test, recurs_test, class_test = divs_with_data_soup
+
+    err_text = "не возващает None, когда надо"
+    assert get_divs_with_data(none_test) is None, err_text
+
+    err_text = (
+        "возващает div внутри div-ов или data-apiary-widget-name не проверяется"
+    )
+    assert len(get_divs_with_data(recurs_test)) == 2, err_text
+
+    err_text = "возващает даже, если class=@marketfront/EmptyIncut"
+    assert len(get_divs_with_data(class_test)) == 1, err_text
 
 
 def fake_get_html_file(session, product_name, market_name):
