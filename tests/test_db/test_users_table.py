83d15ea3e577d593adcc2ea81e7689880dbe34bb
korotkov_egor <korotkovegor10@gmail.com>
2025-03-16 17:58:11 +0300

    add test for users table

---
+++ tests/test_db/test_users_table.py
@@ -0,0 +1,35 @@
+from products_assistent.db import users_table
+from products_assistent.db.conn_to_db import get_db
+
+
+def test_save_user(app):
+    with app.app_context():
+        db = get_db()
+        reqs_repo = users_table.UsersRepo(db)
+
+        assert (
+            reqs_repo.save_user("test", "test_email@gmail.com", "Test_pass123!")
+            == "Ошибка при сохранении пользователя: UNIQUE constraint failed: users.email"
+        )
+
+        assert (
+            reqs_repo.save_user(
+                "test", "test_email@gmail.right", "Test_pass123!"
+            )
+            is None
+        )
+
+
+def test_get_user_by_email(app):
+    with app.app_context():
+        db = get_db()
+        reqs_repo = users_table.UsersRepo(db)
+
+        assert reqs_repo.get_user_by_email("Empty") is None
+
+        assert reqs_repo.get_user_by_email("test_email@gmail.com")[:-2] == (
+            1,
+            "test_name",
+            "test_email@gmail.com",
+            "Test_pass123!",
+        )
